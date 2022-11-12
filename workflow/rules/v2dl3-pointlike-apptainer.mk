baaaed34fbe96516a15608bb7eff78dbeee64fa8
GernotMaier <gernot.maier@desy.de>
2022-11-12 16:43:36 +0100

    pointlike v2dl3

--- workflow/rules/v2dl3-pointlike-apptainer.mk
+++ workflow/rules/v2dl3-pointlike-apptainer.mk
@@ -3,13 +3,12 @@ rule v2_dl3_pointlike:
 		anasum_file="data/input/{runnumber}.anasum.root",
 		effarea_file="data/input/effectiveArea.root"
 	output: 
-		"data/output/{runnumber}.fits.gz"
+		"results/{runnumber}.fits.gz"
 	log:
-		"data/logs/{runnumber}.v2ld3_pointlike.log"
+		"logs/{runnumber}.v2ld3_pointlike.log"
+	container:
+		"docker://ghcr.io/veritas-observatory/v2dl3:pr-171-v2dl3-eventdisplay"
 	shell:
-		'''
-		apptainer exec docker://ghcr.io/veritas-observatory/v2dl3:pr-171-v2dl3-eventdisplay \
-			python /V2DL3/pyV2DL3/script/v2dl3_for_Eventdisplay.py \
-			-f {input.anasum_file} {input.effarea_file} \
-			{output}
-		'''
+		"python /V2DL3/pyV2DL3/script/v2dl3_for_Eventdisplay.py "
+		"-f {input.anasum_file} {input.effarea_file} "
+		"{output}"
